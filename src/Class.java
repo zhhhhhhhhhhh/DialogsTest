65ebd297dddf2c5679ef773ed2a29881514c2299
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-09-18 17:07:32 +0300

    add Class

---
+++ src/Class.java
@@ -0,0 +1,63 @@
+public class Class {
+
+    private final String name;
+
+    private final String surname;
+
+    private final Integer age;
+
+    private final String a;
+
+    private final String b;
+
+    private final Integer c;
+
+
+    public Class(String name, String surname, Integer age, String a, String b, Integer c) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public Integer getC() {
+        return c;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", a='" + a + '\'' +
+                ", b='" + b + '\'' +
+                ", c=" + c +
+                '}';
+    }
+
+
+}
--- src/Class3.java
+++ src/Class3.java
@@ -1,3 +1,8 @@
 public class Class3 {
     public void foo(){}
+
+    public static void main(String[] args) {
+        Class man = new Class("a", "b", 23, "d", "e", 12);
+        System.out.println(man.toString());
+    }
 }
