159fb9b4987042e0f7920b9af6b1ef9b665dfc94
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-09-23 15:03:49 +0300

    ккк

---
+++ src/ClassTest.java
@@ -0,0 +1,40 @@
+import org.testng.annotations.*;
+
+public class ClassTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetName() {
+    }
+
+    @Test
+    public void testGetSurname() {
+    }
+
+    @Test
+    public void testGetAge() {
+    }
+
+    @Test
+    public void testGetA() {
+    }
+
+    @Test
+    public void testGetB() {
+    }
+
+    @Test
+    public void testGetC() {
+    }
+
+    @Deprecated
+    public void testTestToString() {
+    }
+}
