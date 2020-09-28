4a2cfb5c86f15cdca0a64c908b08a0ca5f2a5998
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-09-28 22:53:01 +0300

    test

--- src/Class3.java
+++ src/Class3.java
@@ -2,7 +2,7 @@ public class Class3 {
     public void foo(){}
 
     public static void main(String[] args) {
-        Class man = new Class("a", "b", 23, "d", "e", 12);
+        Class man = new Class("a", "b", 23, "d", "e", 12, 1);
         System.out.println(man.toString());
     }
 }
