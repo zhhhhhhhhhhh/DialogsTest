d61a2f6a4fb91035dd27c246cbe8b639b9b80028
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-10-22 17:46:03 +0300

    commit in master in clone2
    Signed-off-by: Veronika.Timkina <Veronika.Timkina@jetbrains.com>

--- src/Class3_renamed.java
+++ src/Class3_renamed.java
@@ -1,6 +1,6 @@
 public class Class3_renamed {
     public static void main(String[] args) {
-        System.out.println("уреререр");
+        System.out.println("wrgergergerg");
     }
 
     public void foo(){}
