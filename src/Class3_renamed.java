a56d7075f4f0375bca167a5936a5060fb680fa5b
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-10-22 21:27:06 +0300

    change Class3_ren in clone in testnewbranchfromcurrent_ren
    Signed-off-by: Veronika.Timkina <Veronika.Timkina@jetbrains.com>

--- src/Class3_renamed.java
+++ src/Class3_renamed.java
@@ -1,6 +1,6 @@
 public class Class3_renamed {
     public static void main(String[] args) {
-        System.out.println("кпукпук");
+        System.out.println("ethrthrthrt");
     }
 
     public void foo(){}
