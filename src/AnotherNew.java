9a4b65ec70f64df0238e42606dece37b1b8440c3
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-10-23 16:07:48 +0300

    add Class30 AnotherNew NewClass
    Signed-off-by: Veronika.Timkina <Veronika.Timkina@jetbrains.com>

---
+++ src/AnotherNew.java
@@ -0,0 +1,13 @@
+public class AnotherNew {
+    private final Integer a;
+
+    private final Integer b;
+
+    private final Integer c;
+
+    public AnotherNew(Integer a, Integer b, Integer c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
