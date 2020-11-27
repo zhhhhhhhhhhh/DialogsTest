a745c2420d6fb10ef7021bb58b7f6d881c7c11a4
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-11-27 16:15:30 +0300

    commit in master

--- src/Class3_renamed.java
+++ src/Class3_renamed.java
@@ -1,5 +1,5 @@
 public class Class3_renamed {
     public static void main(String[] args) {
-        System.out.println("56г56геркеркеркеркер56г56г56г");
+        System.out.println("ыыауацауау");
     }
 }
