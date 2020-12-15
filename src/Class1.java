a787ef3def78bc2de736a6f5c7bc94be9b5a3786
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-12-15 18:38:38 +0300

    test commit msg JDK-6956668

--- src/Class1.java
+++ src/Class1.java
@@ -1,5 +1,5 @@
 public class Class1 {
     public static void main(String[] args) {
-        System.out.println("wdwdwhefrfwrgefwewfwefwefwefererefhhhedwd");
+        System.out.println("wdwddwd");
     }
 }
