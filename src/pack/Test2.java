3137d0ebd7481e766a083a47f4410c10b32788f5
Veronika.Timkina <Veronika.Timkina@jetbrains.com>
2020-12-21 17:25:14 +0300

    test comment from modal

---
+++ src/pack/Test2.java
@@ -0,0 +1,40 @@
+package pack;
+
+public class Test2 {
+    /*
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    0
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    0
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    0
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
