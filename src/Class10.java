ccc88bba8ea98d0f2b28dec73d7dce667c7d28f3
Veronika.Timkina <veronika.timkina@jetbrains.com>
2021-03-05 14:24:59 +0300

    IDEA-250689 Check new patch/shelf format

--- src/Class10.java
+++ src/Class10.java
@@ -1,5 +1,5 @@
 public class Class10 {
     public static void main(String[] args) {
-        System.out.println("кгоувпитаптап56нг56г");
+        System.out.println("кгоувпитыфсфысфысаптап56нг56г");
     }
 }
