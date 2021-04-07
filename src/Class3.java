884fddf44951ae76b663ffb33efd8c66c57ef04a
Veronika.Timkina <veronika.timkina@jetbrains.com>
2021-04-07 20:49:33 +0300

    analyze code stage

--- src/Class3.java
+++ src/Class3.java
@@ -3,6 +3,6 @@ public class Class3 {
 
     public static void main(String[] args) {
         Class man = new Class("a", "b", 23, "d", "e", 12, 1, "e");
-        System.out.println(man + "gggggрркурукруррggggggg");
+        System.out.println(man + "вкрукрркркрку");
     }
 }
