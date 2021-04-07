2f333015620230d04348808d98f98afd19f10abb
Veronika.Timkina <veronika.timkina@jetbrains.com>
2021-04-07 19:03:17 +0300

    check imports stage

--- src/Class3_renamed.java
+++ src/Class3_renamed.java
@@ -2,7 +2,7 @@ public class Class3_renamed {
 
 
     public static void main(String[] args) {
-        System.out.println("курукрукрукр");
+        System.out.println("куруыппукпкпкрукрукр");
     }
 
 
