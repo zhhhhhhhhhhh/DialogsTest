5fc9ac4505fcd97783b709bb95c611a32b662466
Veronika.Timkina <veronika.timkina@jetbrains.com>
2021-04-07 15:08:44 +0300

    check rearrange stage

---
+++ src/MyClass.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    int field;
+
+    public static void main(String[] args) {
+        //blaah
+    }
+
+    public int getField() {
+        return field;
+    }
+
+    public void setField(int field) {
+        this.field = field;
+    }
+}
