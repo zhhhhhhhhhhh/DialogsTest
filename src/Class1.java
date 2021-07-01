69c2243d696443b365bd14286cb8abe91c42a35c
Veronika.Timkina <veronika.timkina@jetbrains.com>
2021-07-01 18:47:06 +0300

    4

--- src/Class1.java
+++ src/Class1.java
@@ -1,6 +1,6 @@
 public class Class1 {
     //rerfergergerg
     public static void main(String[] args) {
-        System.out.println("wdweeeeукпуedwedwedкпукaaaaaaeeeqdweded");
+        System.out.println("wd");
     }
 }
