a5f32087b012cc06ff129a0baf743dcd35177f00
Agent Cooper <test1@q.qq>
2021-08-03 17:54:28 +0300

    commit with key

--- src/Class1.java
+++ src/Class1.java
@@ -1,6 +1,6 @@
 public class Class1 {
     public static void main(String[] args) {
-        System.out.println("mmmалt35t34tьалаmm");
+        System.out.println("mmmалt3efewfew5t34tьалаmm");
     }
 
     //changes
