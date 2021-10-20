77a26db5813a962aa0034fefb7d61a30e0be405e
veronika <veronika.timkina@jetbrains.com>
2021-10-20 21:57:44 +0300

    pushed

--- src/Class3_renamed.java
+++ src/Class3_renamed.java
@@ -1,9 +1,5 @@
 public class Class3_renamed {
-
-
     public static void main(String[] args) {
-        System.out.println("куруыппукпкпкрукрукр");
+        System.out.println("куруыпdfewfwefпукпкпкрукрукр");
     }
-
-
 }
