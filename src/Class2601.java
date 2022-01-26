fb18167dc6282810caba715ee8f17784e18579f5
Veronika Timkina <veronika.timkina@jetbrains.com>
2022-01-26 12:30:30 +0300

    commit with GPG and build status

---
+++ src/Class2601.java
@@ -0,0 +1,78 @@
+public class Class2601 {
+
+    private final String name;
+
+    private final String surname;
+    private final String b;
+
+    public Class2601(String name, String surname, String b, Integer age, String a, Integer c, Integer d, String e) {
+        this.name = name;
+        this.surname = surname;
+        this.b = b;
+        this.age = age;
+        this.a = a;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+    }
+
+    //ewefwefhtrrhrtui,liol,ioliolioliolio
+    public Integer getD() {
+        return d;
+    }
+
+    private final Integer age;
+
+    private final String a;
+
+    private final Integer c;
+
+    public String getE() {
+        return e;
+    }
+
+    private final Integer d;
+
+    private final String e;
+
+//5hy45y5y45ytjrjyjtjtj
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    //egwewgwegwegwg
+    public Integer getC() {
+        return c;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", a='" + a + '\'' +
+                ", b='" + b + '\'' +
+                ", c=" + c +
+                '}';
+    }
+
+
+}
