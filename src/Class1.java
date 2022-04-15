23bed9dc37df03eeb85dff7f453b08181a74f15c
John Doe <somebody@q.qq>
2022-04-15 12:17:59 +0300

    IDEA-823648 aaaaaaaaa
    Signed-off-by: Veronika Timkina <veronika.timkina@jetbrains.com>

--- src/Class1.java
+++ src/Class1.java
@@ -1,5 +1,5 @@
 public class Class1 {
-    //rerfergergerg
+    //rerfergergergwegwegewegweg
     public static void main(String[] args) {
         System.out.println("mmmалt3efewfew5t34tьалаmm");
     }
