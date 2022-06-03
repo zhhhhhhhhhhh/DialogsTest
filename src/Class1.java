a824cbf484152158a6e73e623b43e2054ca24b36
Veronika Timkina <veronika.timkina@jetbrains.com>
2022-06-03 17:29:34 +0300

    changes in Class1 in master
    Signed-off-by: Veronika Timkina <veronika.timkina@jetbrains.com>

--- src/Class1.java
+++ src/Class1.java
@@ -1,7 +1,7 @@
 public class Class1 {
     //rerfergergergwegwegewegweg
     public static void main(String[] args) {
-        System.out.println("mmmалt3efewfew5t34tьалаmm");
+        System.out.println("master");
     }
 
     //changesgbfgbgbfgbrgegergergrgcvcxvxcegsegseesgegevcxvergregegregrg
