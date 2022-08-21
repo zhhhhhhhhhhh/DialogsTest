2faf61a2ca111f01f78078a5bcd5a62cf4632e31
Veronika Timkina <veronika.timkina@jetbrains.com>
2022-08-21 14:13:13 +0200

    11111

--- src/Class1.java
+++ src/Class1.java
@@ -1,5 +1,6 @@
+import alala;
 public class Class1 {
-    //wfwqfwqfwqfwf
+    //wfwqfwqfwqfwfwrg
     public static void main(String[] args) {
         System.out.println("rbegeegwegwegewggergerh");
     }
