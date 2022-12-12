19a280c02e4e9562833a014f043cc9347f1ae0c4
Veronika Timkina <texst@jetbrains.com>
2022-12-12 19:11:36 +0100

    IDEA-111

--- src/Class1.java
+++ src/Class1.java
@@ -2,6 +2,6 @@ import alala;
 public class Class1 {
     //wfwqfwqfwqfwfwrg
     public static void main(String[] args) {
-        System.out.println("rbegeegwegwegewggergerh");
+        System.out.println("rbegeegwegwewvgwevwevwegewggergerh");
     }
 }
