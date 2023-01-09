48ce62972d65fcf6013dec4e76d7c08dbbc39a90
Veronika Timkina <texst@jetbrains.com>
2023-01-09 12:47:12 +0100

    #IDEA-111

--- src/Class.java
+++ src/Class.java
@@ -5,7 +5,7 @@ public class Class {
     private final String surname;
     private final String b;
 
-    //ewefwefhtrrhrtwfqwfwqqwf
+    //ewefwefhtrrhrtwfqwfwqqwfqwdqwdqwdqwd
     public Integer getD() {
         return d;
     }
