68c3d8af18cbd542e2b9b83df7b2f4f9113c4b69
Veronika Timkina <texst@jetbrains.com>
2023-01-11 18:25:29 +0100

    IDEA-111

--- src/Class3.java
+++ src/Class3.java
@@ -1,5 +1,5 @@
 public class Class3 {
-    public void foofoo(){}
+    public void foofoofoo(){}
 
     public static void main(String[] args) {
         Class man = new Class("a", "b", 23, "d", "e", 12, 1);
