8360344780d3ed398f40f31399543351024e6109
Veronika Timkina <veronika.timkina@jetbrains.com>
2023-09-07 12:06:43 +0200

    wrghergergerg

--- src/Class.java
+++ src/Class.java
@@ -5,7 +5,7 @@ public class Class {
     private final String surname;
     private final String b;
 
-    //ewefwefhtrrhrtwfqwfwqqwfqwdqwdqwdqwdwedwedwedwedwedewfewwewef
+    //ewefwefhtrrhrtwfqwfwqqwfqwdqwdqwdqwdwedwedwedwedwedewfewwewefwrgwrgergergerg
     public Integer getD() {
         return d;
     }
