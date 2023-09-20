0a357c487fddf5291f59ba05771e82719e6427af
Veronika Timkina <veronika.timkina@jetbrains.com>
2023-09-20 16:23:52 +0200

    wegwegwegeg

--- src/Class.java
+++ src/Class.java
@@ -5,7 +5,7 @@ public class Class {
     private final String surname;
     private final String b;
 
-    //ewefwefhtrrhrtewfwefwefewefwefewfwef
+    //ewefwefhtrrhrtewfwefwefewefwefewfwefvdfvdfvdfvdfef2wgwgwrggrv
     public Integer getD() {
         return d;
     }
