9de7ba5a327fa1a641fe29fe869a1d8c054dd62a
Veronika.Timkina <timkina.veronika@gmail.com>
2024-12-18 12:18:13 +0100

    asas

--- src/Class1.java
+++ src/Class1.java
@@ -2,6 +2,6 @@ import alala;
 public class Class1 {
     //wfwqfwqfwqfwfwrg
     public static void main(String[] args) {
-        System.out.println("rbegeegwegwewvgwevwevwegewggergerh");
+        System.out.println("rbege54yh54yh45y45egwegwewvgwevwevwegewggergerh");
     }
 }
