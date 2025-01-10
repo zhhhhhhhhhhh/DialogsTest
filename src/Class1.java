64e19d505329d4df8e40c4f7928920efd0af220d
Veronika.Timkina <timkina.veronika@gmail.com>
2025-01-10 12:42:23 +0100

    fixup! try2123123

--- src/Class1.java
+++ src/Class1.java
@@ -1,5 +1,5 @@
 public class Class1 {
-    //wfwqfwqfwqfwgfgr4e3g34tg34twfwrg
+    //wfwqfwqfwqfwgfgr4e3g34tg34twfwrgцукп4цкупкцуп
     public static void main(String[] args) {
         System.out.println("rbegeegwegwewvgwevwevwegewggergerh");
     }
