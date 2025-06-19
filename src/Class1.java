6e2502ab818ba52c0c311a96e900167a9aa1d34a
Veronika.Timkina <timkina.veronika@gmail.com>
2025-06-19 16:23:35 +0200

    123wefwefwef

--- src/Class1.java
+++ src/Class1.java
@@ -1,8 +1,7 @@
 public class Class1 {
     //12343t34t34t
     //123
-    //wfwqfrrrrrrrrrrrrwqfwqfwgfgr4e3g34tg34twfwrgцукп4цкупкцуп
     public static void main(String[] args) {
-        System.out.println("rbegeegwegwewvgwevwevwegewggergerh");
+        System.out.println("ewfefwefwef");
     }
 }
